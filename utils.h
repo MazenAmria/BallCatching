dfe885b546d108f9c935537ec1aea4173e28d1f6
MazenAmria <t.mazenamria@gmail.com>
2021-05-30 19:41:57 +0300

    Edits

--- utils.h
+++ utils.h
@@ -4,26 +4,25 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-#define N_PLAYERS 3
+#define N_PLAYERS 4
 
 typedef struct player
 {
-	pthread_t thread;   /* descriptor of the player's thread */
-	pthread_cond_t rcv; /* indicates that ball has been recieved */
-	int height;	    /* height of the player */
+	pthread_t thread;    /* descriptor of the player's thread */
+	pthread_cond_t rcv;  /* indicates that ball has been recieved */
+	unsigned int height; /* height of the player */
+	unsigned int skcnt;  /* times this players was seeker */
 } * player_t;
 
 typedef struct ball
 {
-	player_t src;  /* the player that throws the ball */
-	player_t dest; /* the player that the ball thrown to */
-	int height;    /* height of the ball */
+	player_t src;	     /* the player that throws the ball */
+	player_t dest;	     /* the player that the ball thrown to */
+	unsigned int height; /* height of the ball */
 } * ball_t;
 
-typedef player_t seeker_t;
-
-seeker_t SEEKER;   /* seeker's descriptor */
-player_t *PLAYERS; /* list of players descriptors */
-ball_t BALL;	   /* ball's descriptor */
+player_t PLAYERS[N_PLAYERS]; /* list of players descriptors */
+ball_t BALL;		     /* ball's descriptor */
+pthread_cond_t end_game;     /* indicates game end */
 
 #endif /* __UTILS_H__ */
